1f8dc6226f81b75430d8528f295e96eefdfdab1f
Vishnu Tej Nanda Kumar <vishnutejnk7@gmail.com>
2025-05-22 01:46:32 -0300

    Fix styling

--- src/Components/AboutMeSection.tsx
+++ src/Components/AboutMeSection.tsx
@@ -2,7 +2,7 @@ import { Briefcase, Code, User } from "lucide-react"
 
 export const AboutMeSection = () => {
     return (
-        <section id="#about" className="py-24 px-4 relative">
+        <section id="about" className="py-24 px-4 relative">
             <div className="container mx-auto max-w-5xl">
                 <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
                     About <span className="text-primary"> Me</span>
--- src/Components/SkillsSection.tsx
+++ src/Components/SkillsSection.tsx
@@ -88,7 +88,7 @@ const allSkills = [
 export const SkillsSection = () => {
     return (
         <section id="skills" className="py-24 px-4 relative">
-            <div className="container mx-auto max-w-6xl">
+            <div className="container mx-auto max-w-7xl">
                 <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center text-glow">
                     My <span className="text-primary">Skills</span>
                 </h2>
